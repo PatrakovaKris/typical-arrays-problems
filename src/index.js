dc05352c8b2806373f4e48dc6105bb13019d8ef9
PatrakovaKris <patrakovakris@gmail.com>
2020-03-02 23:11:01 +0400

    feat: add function avg

--- src/index.js
+++ src/index.js
@@ -8,5 +8,5 @@ exports.max = function max (array) {
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    return (array === undefined || array.length === 0) ? 0:array.reduce((a, b) => a + b, 0)/array.length;
 }
