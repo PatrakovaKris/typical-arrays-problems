8d61311215e62695abf0b9cbc6c66acc4fbb1886
PatrakovaKris <patrakovakris@gmail.com>
2020-03-02 23:10:31 +0400

    feat: add function max

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 
 exports.min = function min (array) {
-return (array === undefined || array.length === 0)? 0:Math.min.apply(null, array);	
+    return (array === undefined || array.length === 0)? 0:Math.min.apply(null, array);	
 }
 
 exports.max = function max (array) {
-  return 0;
+	return (array === undefined || array.length === 0)? 0: Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
